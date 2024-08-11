b803ec33de56cdd7150d111ba7bed61ff7a84aa8
penguinshivam <shivaxagarwal@gmail.com>
2024-08-11 23:52:07 +0530

    js sring and basic

--- JS/1/Jsintro.js
+++ JS/1/Jsintro.js
@@ -1,5 +1,143 @@
-console.log("hello");
-let prs=10;
-let era=15;
-let output=`the total price is :${prs+era} rupees`;
-console.log(output);
+let pencil = 10;
+let esrasre = 20;
+// let output = "the toatl price is" + (pencil + esrasre) + "Rupee";
+// let output = the total price is :${pencil+esrasre}
+// console.log( output);
+
+// OPERATORS
+// 1.arithmetic
+// 2.uniary
+// 3.assigment
+let a = 10 ; 
+let b = 5;
+let c = a+b;
+// console.log(c );
+
+// 4.comparison
+let age = 18;
+// console.log(age>18);
+// console.log(34>18);
+// console.log(34!=18);
+// console.log(34==18);
+let str = "5";
+let n = 5;
+// console.log(n==str);
+// console.log(0==" ");
+// console.log( 0== false);
+// console.log( 0===false);
+// console.log( 0===" ");
+// unicode a<b<c....<A<B<C....
+// console.log("p"<"P");
+
+// 5.cond statment
+// Ques :taffic light 
+// let color = "red";
+// if( lg ==="red"){
+//     console.log("stop");
+// }
+// if( lg ==="yellow"){
+//     console.log("wait");
+// }
+// if( lg ==="green"){
+//     console.log("Go");
+// }
+
+let first = "Palak";
+// let last = "rehan";
+// let name = first + last;
+// console.log( name.length);
+// // ques
+// let price = "XL";
+// if( price === "XL"){
+//     console.log( "250")
+// }
+// else if( price === "L"){
+//     console.log( "200")
+// }
+// else if ( price === "M"){
+//     console.log( "150")
+// }else{
+//     console.log("100");
+
+// // ques
+// let s = "apple";
+// if( s[0] ==="a"&& s.length>3){
+//     console.log("good");
+// }else{
+//     console.log("bad")
+// }
+
+
+// if(true){
+//     console.log("true");
+// }else{
+//     console.log("false")
+// }
+
+// ques day
+let day = 1;
+// switch(day){
+//     case 1:
+//         console.log("monday");
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+//     case 3:
+//         console.log("wednesday");
+//         break;
+// }
+
+// alert and prompt
+// alert("something is godd")
+// console.error("this is error");
+// console.warn("this is error");
+
+// let f =prompt("enter  first name");
+// let l = prompt("enter last name");
+// alert(${f} iis ${l});
+
+
+// larget of 3
+let x = 10; y=5;z=11;
+// if( x> y){
+//     if(x >z){
+//         console.log(x);
+//     }else{
+//         console.log(z);
+//     }
+// }else{
+//     if(y>z){
+//     console.log(y);
+//     }else{
+//         console.log(z);
+//     }
+// }
+
+// last digit same?
+let n1 = 32;
+let n2 = 4572;
+
+// if((n1[n1.length -1]) ===(n2[n2.length -1])){
+//     console.log("same");
+// }else{
+//     console.log("not")
+// }
+
+
+// string 
+// alice
+let s = "ILove you";
+// console.log(s.trim().s.touppercase());
+console.log(s.slice(0,3));
+console.log(s.slice(0,3));
+
+// replace
+console.log(s.replace("love","hate"));
+
+// repeat
+console.log(s.repeat(5));
+
+// ques
+let msg = "Hello!";
+console.log(msg(msg.trim).toUpperCase());
