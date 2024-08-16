af11b825971da25f44f088817a2d3a45cabdc6e6
penguinshivam <shivaxagarwal@gmail.com>
2024-08-16 23:34:44 +0530

    Js basics 5

--- JS/1/Jsintro.js
+++ JS/1/Jsintro.js
@@ -1,248 +1,287 @@
-let pencil = 10;
-let esrasre = 20;
-// let output = "the toatl price is" + (pencil + esrasre) + "Rupee";
-// let output = the total price is :${pencil+esrasre}
-// console.log( output);
+// let pencil = 10;
+// let esrasre = 20;
+// // let output = "the toatl price is" + (pencil + esrasre) + "Rupee";
+// // let output = the total price is :${pencil+esrasre}
+// // console.log( output);
 
-// OPERATORS
-// 1.arithmetic
-// 2.uniary
-// 3.assigment
-let a = 10 ; 
-let b = 5;
-let c = a+b;
-// console.log(c );
+// // OPERATORS
+// // 1.arithmetic
+// // 2.uniary
+// // 3.assigment
+// let a = 10 ; 
+// let b = 5;
+// let c = a+b;
+// // console.log(c );
 
-// 4.comparison
-let age = 18;
-console.log(age>18);
-console.log(34>18);
-console.log(34!=18);
-console.log(34==18);
-let str = "5";
-let n = 5;
-// console.log(n==str);
-// console.log(0==" ");
-// console.log( 0== false);
-// console.log( 0===false);
-// console.log( 0===" ");
-// unicode a<b<c....<A<B<C....
-// console.log("p"<"P");
+// // 4.comparison
+// let age = 18;
+// console.log(age>18);
+// console.log(34>18);
+// console.log(34!=18);
+// console.log(34==18);
+// let str = "5";
+// let n = 5;
+// // console.log(n==str);
+// // console.log(0==" ");
+// // console.log( 0== false);
+// // console.log( 0===false);
+// // console.log( 0===" ");
+// // unicode a<b<c....<A<B<C....
+// // console.log("p"<"P");
 
-// 5.cond statment
-// Ques :taffic light 
-// let color = "red";
-// if( lg ==="red"){
-//     console.log("stop");
-// }
-// if( lg ==="yellow"){
-//     console.log("wait");
-// }
-// if( lg ==="green"){
-//     console.log("Go");
-// }
+// // 5.cond statment
+// // Ques :taffic light 
+// // let color = "red";
+// // if( lg ==="red"){
+// //     console.log("stop");
+// // }
+// // if( lg ==="yellow"){
+// //     console.log("wait");
+// // }
+// // if( lg ==="green"){
+// //     console.log("Go");
+// // }
 
-let first = "Palak";
-// let last = "rehan";
-// let name = first + last;
-// console.log( name.length);
+// let first = "Palak";
+// // let last = "rehan";
+// // let name = first + last;
+// // console.log( name.length);
+// // // ques
+// // let price = "XL";
+// // if( price === "XL"){
+// //     console.log( "250")
+// // }
+// // else if( price === "L"){
+// //     console.log( "200")
+// // }
+// // else if ( price === "M"){
+// //     console.log( "150")
+// // }else{
+// //     console.log("100");
+
+// // // ques
+// // let s = "apple";
+// // if( s[0] ==="a"&& s.length>3){
+// //     console.log("good");
+// // }else{
+// //     console.log("bad")
+// // }
+
+
+// // if(true){
+// //     console.log("true");
+// // }else{
+// //     console.log("false")
+// // }
+
+// // ques day
+// let day = 1;
+// // switch(day){
+// //     case 1:
+// //         console.log("monday");
+// //         break;
+// //     case 2:
+// //         console.log("tuesday");
+// //         break;
+// //     case 3:
+// //         console.log("wednesday");
+// //         break;
+// // }
+
+// // alert and prompt
+// // alert("something is godd")
+// // console.error("this is error");
+// // console.warn("this is error");
+
+// // let f =prompt("enter  first name");
+// // let l = prompt("enter last name");
+// // alert(${f} iis ${l});
+
+
+// // larget of 3
+// let x = 10; y=5;z=11;
+// // if( x> y){
+// //     if(x >z){
+// //         console.log(x);
+// //     }else{
+// //         console.log(z);
+// //     }
+// // }else{
+// //     if(y>z){
+// //     console.log(y);
+// //     }else{
+// //         console.log(z);
+// //     }
+// // }
+
+// // last digit same?
+// let n1 = 32;
+// let n2 = 4572;
+
+// // if((n1[n1.length -1]) ===(n2[n2.length -1])){
+// //     console.log("same");
+// // }else{
+// //     console.log("not")
+// // }
+
+
+// // string 
+// // alice
+// let s = "ILove you";
+// // console.log(s.trim().s.touppercase());
+// console.log(s.slice(0,3));
+// console.log(s.slice(0,3));
+
+// // replace
+// console.log(s.replace("love","hate"));
+
+// // repeat
+// console.log(s.repeat(5));
+
 // // ques
-// let price = "XL";
-// if( price === "XL"){
-//     console.log( "250")
-// }
-// else if( price === "L"){
-//     console.log( "200")
-// }
-// else if ( price === "M"){
-//     console.log( "150")
-// }else{
-//     console.log("100");
+// let msg = "Hello!";
+// console.log(msg(msg.trim).toUpperCase());
 
-// // ques
-// let s = "apple";
-// if( s[0] ==="a"&& s.length>3){
-//     console.log("good");
-// }else{
-//     console.log("bad")
-// }
-
-
-// if(true){
-//     console.log("true");
-// }else{
-//     console.log("false")
-// }
-
-// ques day
-let day = 1;
-// switch(day){
-//     case 1:
-//         console.log("monday");
-//         break;
-//     case 2:
-//         console.log("tuesday");
-//         break;
-//     case 3:
-//         console.log("wednesday");
-//         break;
-// }
-
-// alert and prompt
-// alert("something is godd")
-// console.error("this is error");
-// console.warn("this is error");
-
-// let f =prompt("enter  first name");
-// let l = prompt("enter last name");
-// alert(${f} iis ${l});
-
-
-// larget of 3
-let x = 10; y=5;z=11;
-// if( x> y){
-//     if(x >z){
-//         console.log(x);
-//     }else{
-//         console.log(z);
-//     }
-// }else{
-//     if(y>z){
-//     console.log(y);
-//     }else{
-//         console.log(z);
-//     }
-// }
-
-// last digit same?
-let n1 = 32;
-let n2 = 4572;
-
-// if((n1[n1.length -1]) ===(n2[n2.length -1])){
-//     console.log("same");
-// }else{
-//     console.log("not")
-// }
-
-
-// string 
-// alice
-let s = "ILove you";
-// console.log(s.trim().s.touppercase());
-console.log(s.slice(0,3));
-console.log(s.slice(0,3));
-
-// replace
-console.log(s.replace("love","hate"));
-
-// repeat
-console.log(s.repeat(5));
-
-// ques
-let msg = "Hello!";
-console.log(msg(msg.trim).toUpperCase());
-
-let cars=["audi1","audi21","audi11","audi3","audi12"];
-console.log(cars);
-cars.splice(0,1,"audi","merc")
-console.log(cars);
-
-// let cars = ['audi','bmw','maruti'];
+// let cars=["audi1","audi21","audi11","audi3","audi12"];
+// console.log(cars);
+// cars.splice(0,1,"audi","merc")
 // console.log(cars);
 
-// find element
-// 6
-let arr = ["hello",'a', 23,34,66,-1];
-let item = 66;
+// // let cars = ['audi','bmw','maruti'];
+// // console.log(cars);
 
-if( arr.indexOf(item) != -1){
-    console.log("found");
-}else{
-    console.log("dosen't exist");
-}
-// 5
-let str1 = prompt("please enter name");
-console.log(`original = ${str1}`);
-console.log(`w/o spaces = ${str1.trim()}`);
-// 4
-let str2 = "ApNaCOLlege";
-let idx = 3;
-if( str2[idx] == str2[idx].toLowerCase()){
-    console.log("lowercase");
-}else{
-    console.log("not lowercase");
-}
+// // find element
+// // 6
+// let arr = ["hello",'a', 23,34,66,-1];
+// let item = 66;
 
-console.log(typeof null);
-let obj={
-    null: true
-};
+// if( arr.indexOf(item) != -1){
+//     console.log("found");
+// }else{
+//     console.log("dosen't exist");
+// }
+// // 5
+// let str1 = prompt("please enter name");
+// console.log(`original = ${str1}`);
+// console.log(`w/o spaces = ${str1.trim()}`);
+// // 4
+// let str2 = "ApNaCOLlege";
+// let idx = 3;
+// if( str2[idx] == str2[idx].toLowerCase()){
+//     console.log("lowercase");
+// }else{
+//     console.log("not lowercase");
+// }
 
-console.log();
+// console.log(typeof null);
+// let obj={
+//     null: true
+// };
 
-let arr=[]
-arr.push(4);
-console.log(arr);
+// console.log();
 
-let count=0;
-function volco (str) {
-    for (let i = 0; i < str.length; i++) {
-        if (str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u') {
-            count++;
-        }
-    }
-    return count;
-}
-let str='abcdefghijklmno';
-// console.log(str.length);
+// let arr=[]
+// arr.push(4);
+// console.log(arr);
 
-console.log(volco(str));
+// let count=0;
+// function volco (str) {
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u') {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// let str='abcdefghijklmno';
+// // console.log(str.length);
 
-let ans="";
+// console.log(volco(str));
 
-function uni (str) {
-    ans=ans+str[0];
-    console.log(ans[0]!=str[4]);
+// let ans="";
+
+// function uni (str) {
+//     ans=ans+str[0];
+//     console.log(ans[0]!=str[4]);
     
-    for (let i = 1; i < str.length; i++) {
-        // console.log(str[i]);
-        if (ans.indexOf(str[i])==-1) {
+//     for (let i = 1; i < str.length; i++) {
+//         // console.log(str[i]);
+//         if (ans.indexOf(str[i])==-1) {
             
-            ans=ans+str.charAt(i);
-        }
+//             ans=ans+str.charAt(i);
+//         }
 
-    }    
-    return ans;
-}
-let s="abcdabcefgggh";
-console.log(uni(s));
-let ans="";
+//     }    
+//     return ans;
+// }
+// let s="abcdabcefgggh";
+// console.log(uni(s));
+// let ans="";
 
-function uni (str) {
-    ans=ans+str[0];
-    console.log(ans[0]!=str[4]);
+// function uni (str) {
+//     ans=ans+str[0];
+//     console.log(ans[0]!=str[4]);
     
-    for (let i = 1; i < str.length; i++) {
-        // console.log(str[i]);
-        let curr = str.charAt(i);
+//     for (let i = 1; i < str.length; i++) {
+//         // console.log(str[i]);
+//         let curr = str.charAt(i);
 
-        for (let j = 0; j < ans.length; j++) {
-            // console.log(curr!=ans.charAt(j));
-            if (curr!=ans.charAt(j)) {
-                ans+=curr;
+//         for (let j = 0; j < ans.length; j++) {
+//             // console.log(curr!=ans.charAt(j));
+//             if (curr!=ans.charAt(j)) {
+//                 ans+=curr;
 
-            }
+//             }
             
-        }
+//         }
 
-    }    
-    return ans;
-}
-let s="abcdabcefgggh";
-console.log(uni(s));
+//     }    
+//     return ans;
+// }
+// let s="abcdabcefgggh";
+// console.log(uni(s));
 
-function rand(s,e){
-    return Math.floor(Math.random()*(e-s+1))+s;
-}
-console.log(rand(4,5));
+// function rand(s,e){
+//     return Math.floor(Math.random()*(e-s+1))+s;
+// }
+// console.log(rand(4,5));
+
+// let arr=[1,2,3,4]
+// let sum=arr.reduce((sum,el)=>(sum+el*el))/arr.length
+// console.log(sum);
+
+// let arr=[1,2,3,4]
+
+// let plus5=arr.map((el)=>el+5)
+// console.log(plus5);
+
+// console.log(arr[0].toUpperCase);
+
+// let arr=['apna',"college"];
+// let tocaps=arr.map((el)=>el.toUpperCase());
+// console.log(tocaps);
+
+
+// let arr=[1,2,3,4]
+// function doubleAndReturnArgs(arr,...args){
+//     args.map((el)=>arr.push(el*2));
+//     return arr;
+// }
+// console.log(doubleAndReturnArgs(arr,1,2,3,6));
+
+
+// let obj1={
+//     name: "shivam",
+//     class:"7",
+//     roll:2
+// }
+// let obj2={
+//     rollNo:"34",
+//     roll:"34"
+// }
+// let mergeobj=(obj1,obj2)=>({...obj1,...obj2});
+// console.log(mergeobj(obj1,obj2));
+
+
+
 
