dc841f3a8bdd26f6abe7be289088fb3794670d2e
penguinshivam <shivaxagarwal@gmail.com>
2024-08-29 23:44:27 +0530

    nodeJs  | setup | Module | Process

---
+++ nodeJs/math.js
@@ -0,0 +1,15 @@
+const sum =(a,b)=>a+b;
+const mul =(a,b)=>a*b;
+const sub =(a,b)=>a-b;
+const div =(a,b)=>a/b;
+const g=9.8;
+const pi=9.8;
+
+let obj={
+    sum:sum,
+    mul:mul,
+    g:g,
+    pi:pi,
+};
+
+module.exports = obj;
---
+++ nodeJs/script.js
@@ -0,0 +1,27 @@
+// // // let n=5;
+
+// // // for (let index = 0; index < n; index++) {
+// // //     console.log("hello",index);
+    
+// // // }
+
+
+// // let args =process.argv;
+
+// // for (let index = 2; index <args.length; index++) {
+// //     console.log("hello to",args[index]);
+// // }
+
+// // // console.log(process.argv);
+
+
+// const somevalue=require("./math")
+
+// console.log(somevalue);
+
+// console.log(somevalue.sum(1,2));
+
+
+const fruits=require("./fruits")
+
+console.log(fruits);
