7c2e9fce1dc5c639b349ba2e3c68d26e6db5fbdd
penguinshivam <shivaxagarwal@gmail.com>
2024-09-06 23:42:21 +0530

    Express Js

---
+++ expressJS/index.js
@@ -0,0 +1,34 @@
+const express=require("express");
+const app=express();
+
+// console.dir(app);
+
+let port=8080
+app.listen(port,()=>{
+    console.log(`app is listing to ${port}`);
+})
+// app.use("/test",(req,res)=>{
+//     // console.log(res);
+    
+//     // console.log("request recived");
+//     // res.send({
+//     //     name:"apple",
+//     //     color:"red",
+
+//     // });
+//     res.send("<h1>hello</h1><ul> <li>apple</li> </ul>");
+
+// });
+// app.get("/",(req,res)=>{
+//     res.send("home page")
+//     //̥ res.send("<h1>hello</h1><ul> <li>apple</li> </ul>");
+
+// });
+// app.get("*",(req,res)=>{
+    
+//     res.send("not page does exist")
+// });
+app.get("/:getFruit",(req,res)=>{
+    let getFruit=req.params.getFruit;
+    res.send(`welcome ${getFruit}`)
+})
