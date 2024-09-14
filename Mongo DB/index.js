ead2823ed3cc22595550ec943d3ce408b50efbe1
penguinshivam <shivaxagarwal@gmail.com>
2024-09-14 23:25:22 +0530

    MongoDb with node

---
+++ Mongo DB/index.js
@@ -0,0 +1,30 @@
+const mongoose=require('mongoose');
+
+// let url="http://localhost:8080/users"
+
+main()
+    .then((res)=>{
+        console.log("success");
+    })
+    .catch((err)=>console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test')
+    
+}
+
+const userSchema =new mongoose.Schema({
+    name:String,
+    email:String,
+    age:Number,
+});
+
+const User =mongoose.model("User",userSchema);
+
+const user2 = new User({
+    name:"eve",
+    email:"hello@gmail.com",
+    age:900,
+});
+user2.save().then((res)=>{console.log(res);
+}) ;
