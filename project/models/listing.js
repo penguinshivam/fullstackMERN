ee37ac23ff78d5d0e6a57bb7fd2ec876dd7b4f93
penguinshivam <shivaxagarwal@gmail.com>
2024-09-25 23:05:13 +0530

    Ejs completed with db

--- project/models/listing.js
+++ project/models/listing.js
@@ -11,7 +11,7 @@ const listingSchema=new Schema({
         type:String,
     },
     image:{
-        type:Object,
+        type:String,
         default:"https://images.unsplash.com/photo-1509233725247-49e657c54213?q=80&w=1949&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set:(v)=>v===""?"https://images.unsplash.com/photo-1509233725247-49e657c54213?q=80&w=1949&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
     },
