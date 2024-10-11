3037a9651e4efe16b113b68d67d4a755f7d12d00
penguinshivam <shivaxagarwal@gmail.com>
2024-10-11 23:57:34 +0530

    add image upload

--- project/routes/review.js
+++ project/routes/review.js
@@ -1,40 +1,12 @@
-const express=require("express");
-const router=express.Router({mergeParams:true});
+const express = require("express");
+const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const Review = require("../models/review.js")
-const { reviewSchema } = require("../schema.js");
-const Listing = require("../models/listing.js")
+const { isLoggedIn, validateReview ,isReviewAuthor} = require("../middleware.js");
+const reviewController=require("../controllers/review.js");
 
 
-const validateReview=(req,res,next)=>{
-    let {error}=reviewSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next()
-    }
-}
+router.post("/",isLoggedIn,validateReview,wrapAsync(reviewController.createReview));
 
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.deleteReview));
 
-router.post("/",validateReview,wrapAsync(async (req,res,next)=>{
-    let listing=await Listing.findById(req.params.id)
-    const newReview=new Review(req.body.review);
-    listing.reviews.push(newReview)
-    await newReview.save();
-    await listing.save();
-    req.flash("success","review added successfully");
-    res.redirect(`/listings/${req.params.id}`);
-}))
-
-router.delete("/:reviewId",wrapAsync(async (req,res,next)=>{
-    
-    await Listing.findByIdAndUpdate(req.params.id,{$pull:{reviews:req.params.reviewId}});
-    await Review.findByIdAndDelete(req.params.reviewId);
-    req.flash("deleted","review deleted successfully");
-    res.redirect(`/listings/${req.params.id}`);
-}))
-
-module.exports=router;
-
+module.exports = router;
